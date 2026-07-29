8cf61a5a61bf4b895281c55e435c3dbc81303085
hhh1171317725-create <hhh1171317725-create@users.noreply.github.com>
2026-07-29 16:53:31 +0800

    Redirect unauthenticated users to login

--- src/main/java/com/rockorca/bi/AuthApiController.java
+++ src/main/java/com/rockorca/bi/AuthApiController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class AuthApiController {
 
   public AuthApiController(SessionService sessions) {
     this.sessions = sessions;
+  }
+
+  @GetMapping("/session")
+  public Map<String, Object> session() {
+    return ReportService.mapOf("authenticated", true);
   }
 
   @PostMapping("/login")
